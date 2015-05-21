04b738c6543b04da322848797ba96e5a767dc90d
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-05-21 17:02:38 +0200

    YetiForce Portal ver. 2.0.0

--- config/version.php
+++ config/version.php
@@ -2,5 +2,5 @@
 /* {[The function is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 /* {[Contributor(s): }] */
 
-$version = '1.4.11 RC';
+$version = '2.0.0';
 $patch_version = '20150521';
