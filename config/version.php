e27d74fa3b1e54fa6aaeb815bf622c5c3539e575
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-06-29 13:41:17 +0200

    Update NuSOAP

--- config/version.php
+++ config/version.php
@@ -2,5 +2,5 @@
 /* {[The function is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 /* {[Contributor(s): }] */
 
-$version = '3.1.0';
-$patch_version = '20160518';
+$version = '3.1.1';
+$patch_version = '20160629';
