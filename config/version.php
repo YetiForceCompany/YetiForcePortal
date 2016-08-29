5dcd16b44125799e1a7725e2150a9349bc0fd514
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2016-08-29 16:13:01 +0200

    YetiForce Portal ver. 3.2.0

--- config/version.php
+++ config/version.php
@@ -2,5 +2,5 @@
 /* {[The function is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 /* {[Contributor(s): }] */
 
-$version = '3.1.3';
-$patch_version = '20160729';
+$version = '3.2.0';
+$patch_version = '20160829';
