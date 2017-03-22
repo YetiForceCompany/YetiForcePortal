7f9451df44c277bd7f6931f26a5fc48acdea5c90
salvofan <salvatore@fanara.eu>
2017-03-22 14:04:49 +0000

    Create it_it.lang.php
    The "tickets" are translated into "richieste" which is a word with a very wide meaning. It can be for general support or to put in "problems". for a sales system it would be perfect as in general the potential customer has "richieste" to ask.

---
+++ language/it_it.lang.php
@@ -0,0 +1,134 @@
+<?php
+
+/*+***********************************************************************************************************************************
+
+ * The contents of this file are subject to the YetiForce Public License Version 1.1 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is YetiForce.
+ * The Initial Developer of the Original Code is YetiForce. Portions created by YetiForce are Copyright (C) www.yetiforce.com. 
+ * All Rights Reserved.
+ * Contributor(s): 
+
+ *************************************************************************************************************************************/
+
+global $app_strings;
+$app_strings = Array(
+//Main
+'LBL_CUSTOMER_PORTAL'=>'Portale Clienti',
+'LBL_NAVBAR_TITLE'=>'YetiForce - Portale Clienti',
+'LBL_CLOSE' => 'Chiudi',
+
+//Menu
+'LBL_TOGGLE_NAVIGATION' => 'Cambia menu',
+'LBL_BTN_LNK_LOGOUT'=>'Esci',
+'LBL_BTN_CHANGE_PASSWORD'=>'Cambia password',
+'LBL_HOME'=>'Moduli disponibili',
+'LBL_DASHBOARD'=>'Dashboard',
+
+//Login
+'LBL_SIGN_IN_TITLE'=>'Accesso',
+'LBL_LOGIN'=>'Entra',
+'LBL_PASSWORD'=>'Password',
+'LBL_SIGN_IN' => 'Registrati',
+'LBL_FORGOT_LOGIN'=>'Hai dimenticato la password?',
+'LBL_RESET_PASSWORD' => 'Cambia Password',
+'LBL_EMAIL'=>'Email',
+'LBL_LNK_HOME'=>'Home page',
+'LBL_START' => 'ENTRA',
+'LBL_FORGOT_PASSWORD' => 'Hai dimenticato la password?',
+'LBL_SELECT_LANGUAGE' => 'Seleziona lingua',
+
+//Changing the password
+'LBL_LOGIN'=>'Entra',
+'LBL_CHANGING_PASSWORD' => 'Cambia password',
+'LBL_OLD_PASSWORD'=>'Vecchia password',
+'LBL_NEW_PASSWORD'=>'Nuova password',
+'LBL_CONFIRM_PASSWORD'=>'Conferma password',
+'LBL_BTN_CHANGE_PASSWORD' => 'Cambia password',
+'LBL_INVALID_USERNAME_OR_PASSWORD' => 'Nome utente o password non valida.',
+'LBL_ENTER_NEW_PASSWORDS_SAME' => 'Le passwords non sono uguali',
+'LBL_PASSWORD_CHANGED'=>'La password é stata cambiata.',
+   
+
+//forgot the password
+'LBL_SEND_PASSWORD' => 'Invia',
+'LBL_PASSWORD_HAS_BEEN_SENT' => 'Una nuova password é stata inviata via email, puoi controllare la posta in arrivo.',
+'LBL_NO_MAIL_ADDRESS' => 'Nessun indirizzo email inserito.',
+'LBL_EMAIL_ADDRESS_NOT_FOUND' => 'Indirizzo email non trovato',
+'LBL_LOGIN_REVOKED' => 'Accesso bloccato per questo utente',
+'LBL_MAIL_COULDNOT_SENT' => 'Errore invio email, contatta l'amministratore per risolvere un eventuale errore di configurazione con la tua posta.',
+
+
+//List
+'LBL_NO_RECORDS_FOUND' => 'Nessun elemento trovato in questo modulo',
+'LBL_NO_HELPDESK_RECORDS_FOUND' => 'Nessuna richiesta trovata',
+'LBL_NEW_TICKET'=>'Crea nuova richiesta',
+'LBL_NEW_TICKET_TITLE'=>'Titolo della nuova richiesta',
+'LBL_TICKET_DETAIL'=>'Dettagli richiesta',
+'LBL_HELPDESK_TITLE'=>'Titolo',
+'LBL_HELPDESK_TITLE_HOLDER'=>'Inserisci titolo richiesta',
+'LBL_HELPDESK_SEVERITY'=>'Gravitá',
+'LBL_HELPDESK_CATEGORY'=>'Categoria',
+'LBL_HELPDESK_CONTRACT_SERVICE'=>'Contratto',
+'LBL_HELPDESK_PRIORITY'=>'Prioritá',
+'LBL_HELPDESK_DESCRIPTION'=>'Descrizione',
+
+'LBL_FAQ_CATEGORIES'=>'Categorie',
+'LBL_NO_FAQ_RECORDS_FOUND' => 'Nessuna Domanda&Risposta trovata',
+'LBL_FAQ_ANSWER'=>'Rispondi',
+'LBL_FAQ_RELATED_PRODUCT'=>'Prodotto',
+'LBL_FAQ_ATTACHMENTS'=>'Allegati',
+
+'LBL_PRODUCTS_RELATED_PRODUCTS'=>'Prodotti simili',
+'LBL_PRODUCTS_RELATED_QUOTES'=>'Prodotti relativi ai preventivi',
+'LBL_PRODUCTS_RELATED_INVOICES'=>'Prodotti relativi alle fatture',
+'LBL_SERVICES_RELATED_SERVICES'=>'Servizi simili',
+'LBL_SERVICES_RELATED_QUOTES'=>'Servizi relativi ai preventivi',
+'LBL_SERVICES_RELATED_INVOICES'=>'Servizi relativi alle fatture',
+
+'LBL_RECORDS_PER_PAGE'=>'_MENU_ elementi per pagina',
+'LBL_SHOWING_PAGE'=>'Pagina _PAGE_ di _PAGES_',
+'LBL_NOTHING_FOUND'=>'Nessun elemento trovato',
+'LBL_FILTERED_TOTAL_RECORDS'=>'_MAX_ elementi filtrati',
+'LBL_PROCESSING'=>'Attendere prego ...',
+'LBL_SEARCH'=>'Cerca',
+'LBL_FIRST'=>'Prima',
+'LBL_PREVIOUS'=>'Precedente',
+'LBL_NEXT'=>'Prossima',
+'LBL_LAST'=>'Ultima',
+
+//Detail
+'LBL_HELPDESK_STATUS'=>'Stato della richiesta',
+'LBL_HELPDESK_CLOSE_TICKET'=>'Chiudi richiesta',
+'LBL_HELPDESK_ATTACHMENTS'=>'Allegati',
+'LBL_HELPDESK_UPLOAD'=>'Invia',
+'LBL_HELPDESK_COMMENTS'=>'Commenti alla richiesta',
+'LBL_HELPDESK_SEND_COMMENT'=>'Aggiungi commento',
+'LBL_HELPDESK_DOWNLOAD_ATTACHMENTS'=>'Scarica file',
+
+'LBL_PROJECT_RELATED_TICKETS'=>'Richieste simili', 
+'LBL_PROJECT_RELATED_PROJECTTASK'=>'Tasks relativi al progetto',
+'LBL_PROJECT_RELATED_PROJECTMILESTONE'=>'Traguardi principali relativi al progetto',
+
+'LBL_INVENTORY_PRODUCTS'=>'Prodotti e Servizi',
+'LBL_INVENTORY_NAME'=>'Nome',
+'LBL_INVENTORY_QTY'=>'Quantitá',
+'LBL_INVENTORY_LISTPRICE'=>'Lista prezzi',
+'LBL_INVENTORY_NETPRICE'=>'Prezzo',
+'LBL_INVENTORY_TAX'=>'Tassa',
+
+'LBL_NOTSET_UPLOAD_DIR' => 'Directory temporanea non impostata "upload_dir"',
+'LBL_UPLOAD_FILE_LARGE'=>'Il file é troppo grande',
+'LBL_PROBLEM_UPLOAD'=>'Errore invio file',
+'LBL_FILE_HAS_NO_CONTENTS'=>'Il file non ha alcun contenuto',
+'LBL_ADD_ATTACHMENT' => 'Aggiungi allegato',
+'LBL_COMMENT_AREA' => 'Area per i commenti',
+
+//Widgets
+'LBL_WIDGET_OPENTICKETS' => 'Casi aperti',
+'LBL_WIDGET_TICKETSBYSTATUS' => 'Tickets By Status',
+'LBL_WIDGET_TICKETSSUMTIME' => 'Richieste - Tempo di lavoro diviso per utenti',
+'LBL_WIDGET_PROJECTSUMTIME' => 'Progetti - Tempo di lavoro diviso per progetti',
+);
